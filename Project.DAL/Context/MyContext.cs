2ffa4997b44fef4020c2de9c0fc7ceee297eb0a6
Mustafa Beyaz <mustafabey040@outlook.com>
2024-04-29 19:04:33 +0300

    MyContext.cs added

---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.DAL.Configurations;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Context
+{
+    public class MyContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public MyContext(DbContextOptions<MyContext> options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new AppointmentConfiguration());
+            builder.ApplyConfiguration(new ClinicConfiguration());
+            builder.ApplyConfiguration(new ClinicListConfiguration());
+            builder.ApplyConfiguration(new DoctorConfiguration());
+            builder.ApplyConfiguration(new HospitalConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new UserProfileConfiguration());
+        }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Clinic> Clinics { get; set; }
+        public DbSet<ClinicList> ClinicLists { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
+    }
+}
--- Project.DAL/obj/Debug/net8.0/Project.DAL.AssemblyInfo.cs
+++ Project.DAL/obj/Debug/net8.0/Project.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d65c67567419f1b3f4ac1f17ca38afc5f26e144")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f8c95752b7f6a02ce3b0eb594f85d7365cc1c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
